27be43da8f8c50a8ac3f7722e03303d83d787989
kburczak <133333892+kburczak@users.noreply.github.com>
2023-06-18 20:15:56 +0200

    dodanie sprawdzanai trendu z ostatnich 5 dni

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+
 eur = 100
 usd = 100
-eur_to_usd = [1.0903, 1.0920, 1.0914, 1.0869, 1.0916, 1.0997, 1.1051, 1.0985, 1.0925]
+eur_to_usd = []
 trend = 0
+z_5 = 0
+
+for ilosc in range(1, 100):
+    nowy = float(input())
+    eur_to_usd.append(nowy)
+
 
 for i in range(1, len(eur_to_usd)):
     if eur_to_usd[i] > eur_to_usd[i-1]:
@@ -12,16 +19,34 @@ for i in range(1, len(eur_to_usd)):
         if trend > 0:
             trend = 0
         trend = trend - 1
+    if i >= 5:
+        for a in range(i-5, i):
+            if eur_to_usd[a] > eur_to_usd[a-1]:
+                z_5 = z_5 + 1
+            elif eur_to_usd[a] < eur_to_usd[a-1]:
+                z_5 = z_5 - 1
 
-    if trend > 0:
-        eur = eur + usd / eur_to_usd[i]
-        usd = 0
-    elif trend < 0:
-        usd = usd + eur * eur_to_usd[i]
-        eur = 0
+    if z_5 == 0:
+        if trend > 0:
+            eur = eur + usd / eur_to_usd[i]
+            usd = 0
+        elif trend < 0:
+            usd = usd + eur * eur_to_usd[i]
+            eur = 0
+    else:
+        if z_5 < 0:
+            eur = eur + usd / eur_to_usd[i]
+            usd = 0
+        elif z_5 > 0:
+            usd = usd + eur * eur_to_usd[i]
+            eur = 0
+
 
 print("całość na początku w dolarach", 100 + 100 * eur_to_usd[0])
 print("całość na początku w euro", 100 + 100 / eur_to_usd[0])
 
 print(f'euro: {eur}')
 print(f'dolary: {usd}')
+
+
+
